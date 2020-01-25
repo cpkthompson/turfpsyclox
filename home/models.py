a45f2f0e1fb311ef12e8d18f0520ef348fcdc516
Charles Thompson <cpkthompson@gmail.com>
2020-01-25 06:38:36 +0000

    home page improvements

--- home/models.py
+++ home/models.py
@@ -2,4 +2,8 @@ from wagtail.core.models import Page
 
 
 class HomePage(Page):
+    def get_context(self, request):
+        context = super().get_context(request)
+        context['services'] = ['Financial Advisory', 'Strategy and Operations', 'Valuations']
+        return context
     pass
