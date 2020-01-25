b293d17b86ca87903a7fa5ffa2ea51c689f263f5
Charles Thompson <cpkthompson@gmail.com>
2020-01-25 23:38:33 +0000

    page improvements

--- home/models.py
+++ home/models.py
@@ -6,4 +6,24 @@ class HomePage(Page):
         context = super().get_context(request)
         context['services'] = ['Financial Advisory', 'Strategy and Operations', 'Valuations']
         return context
+
+
+class ServicesPage(Page):
     pass
+
+
+class InsightPage(Page):
+    pass
+
+
+class InsightIndexPage(Page):
+    pass
+
+
+class AboutPage(Page):
+    pass
+
+
+class ContactPage(Page):
+    pass
+
